c4f4292df179ae7314c1d24c53d686e0d263ab6b
oracle-py <mgbemiracle5@gmail.com>
2026-06-28 23:59:07 +0100

    Add Render URL to ALLOWED_HOSTS

--- backend/school_monitoring_system/settings.py
+++ backend/school_monitoring_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c-2x@vc#f$l%4(z7u!ue9cmxqe84emaqy5&hjfd_z7j6g(gvi#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sms-bv3a.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
